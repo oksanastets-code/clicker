869c15c7a7ff74794ebb801ce542fb61bb229469
Oksana Stets <oksanastets73@gmail.com>
2023-04-10 00:18:37 +0300

    add counter

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log('Hello');
+const refs = {
+    counterValue: document.querySelector('#value'),
+    scoreValue: document.querySelector('#score'),
+    plusBtn: document.querySelector('[data-action="increment"]'),
+};
+let total = Number(refs.counterValue.textContent);
+let totalScore = Number(refs.scoreValue.textContent);
+
+const increment = () => {
+    total += 1;
+    refs.counterValue.textContent = total;
+
+if (total === 5) {
+    countScore();
+    } 
+
+};
+
+refs.plusBtn.addEventListener('click', increment);
+
+const countScore = () => {
+    totalScore = totalScore + total;
+    refs.scoreValue.textContent = totalScore;
+    total = 0;
+    refs.counterValue.textContent = total;
+}
+
+
+
