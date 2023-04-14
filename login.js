865c975db31779ecbfc05b50267eb327ba29adde
Oksana Stets <oksanastets73@gmail.com>
2023-04-14 10:37:44 +0300

    add lightbox with login

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 console.log('login');
-
+const lightbox = document.querySelector('.js-lightbox');
 class Login {
     constructor(form, fields) {
         this.form = form;
@@ -19,18 +19,17 @@ class Login {
                 error++;
             }
         });
-        // if everything validates, error will be 0 and can continue
         if (error == 0) {
         console.log('success');
         //     localStorage.setItem("auth", 1);
-        //     this.form.submit();
+            this.form.submit();
+            lightbox.classList.remove('is-open');
+
         }
     });
     }
     validateFields(field) {
-    // remove any whitespace and check to see if the field is blank, if so return false
     if (field.value.trim() === "") {
-        // set the status based on the field, the field label, and if it is an error message
         this.setStatus(
             field,
             `${field.previousElementSibling.innerText} cannot be blank`,
@@ -38,38 +37,39 @@ class Login {
         );
         return false;
     } else {
-        if (field.type == "password") {
-            if (field.value.length < 8) {
+        if (field.id == "email") {
+            console.log(field.value);
+            const mail = field.value;
+            const regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+            const isMatch = mail.match(regEx);
+            console.log(isMatch);
+            if (!isMatch) {
                 this.setStatus(
                     field,
-                    `${field.previousElementSibling.innerText} must be at least 8 characters`,
+                    `${field.previousElementSibling.innerText} needs more special symbols`,
                     "error"
                 );
                 return false;
             } else {
-                // set the status based on the field without text and return a success message
                 this.setStatus(field, null, "success");
                 return true;
             }
         } else {
-            // set the status based on the field without text and return a success message
             this.setStatus(field, null, "success");
             return true;
         }
     }
     }
     setStatus(field, message, status) {
-        // create variable to hold message
         const errorMessage = field.parentElement.querySelector(".error-message");
 
-        // if success, remove messages and error classes
         if (status == "success") {
             if (errorMessage) {
                 errorMessage.innerText = "";
             }
             field.classList.remove("input-error");
         }
-        // if error, add messages and add error classes
+        // add class
         if (status == "error") {
             errorMessage.innerText = message;
             field.classList.add("input-error");
@@ -78,6 +78,6 @@ class Login {
 }
 const form = document.querySelector('.loginForm');
 if (form) {
-    const fields = ["username", "password"];
+    const fields = ["username", "email"];
     const validator = new Login(form, fields);
 }
